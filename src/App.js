710630b42f452b1c8c00980373e79f1b553d7e93
Sagar Acharya <sagar_acharya@live.in>
2019-06-21 11:28:31 +0530

    == issue

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   nameChangedHandler = (event,id) => {
 	//console.log('was clicked');
 	const personIndex = this.state.persons.findIndex(p => {
-		return p.id = id;
+		return p.id == id;
 	});
 
 	const person = { ...this.state.persons[personIndex] }
